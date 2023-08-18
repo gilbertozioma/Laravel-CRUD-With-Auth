b482e4d52092dcc43493fef9cc10d77750e5434a
Your Name <gilbertozioma0@gmail.com>
2023-08-18 14:31:48 +0100

    bg-dark, back button

--- resources/views/product/read.blade.php
+++ resources/views/product/read.blade.php
@@ -4,7 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="mt-5 mb-3 clearfix">
+                <a href="{{ url('/') }}" class=" btn btn-sm btn-outline-primary float-end"><i class="fa fa-arrow-left"></i> Back</a>
+            </div>
+            <div class="card bg-dark text-light border-secondary">
                 <div class="card-header">{{ __('Product Details') }}</div>
 
                 @if ($product)
@@ -12,21 +15,21 @@
                 <div class="card-body">
                     <div class="form-group mb-3">
                         <label for="name">{{ __('Name') }}</label>
-                        <input id="name" type="text" class="form-control" value="{{ $product->name }}" readonly>
+                        <input id="name" type="text" class="form-control bg-dark text-light border-secondary" value="{{ $product->name }}" readonly>
                     </div>
 
                     <div class="form-group mb-3">
                         <label for="description">{{ __('Description') }}</label>
-                        <textarea id="description" class="form-control" readonly>{{ $product->description }}</textarea>
+                        <textarea id="description" class="form-control bg-dark text-light border-secondary" readonly>{{ $product->description }}</textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="price">{{ __('Price') }}</label>
-                        <input id="price" type="text" class="form-control" value="{{ $product->price }}" readonly>
+                        <input id="price" type="text" class="form-control bg-dark text-light border-secondary" value="{{ $product->price }}" readonly>
                     </div>
                 </div>
                 @else
-                    <h4 class="alert-danger">Error</h4>
+                    <h4 class="alert-danger">Something Went Wrong</h4>
                 @endif
             </div>
         </div>
