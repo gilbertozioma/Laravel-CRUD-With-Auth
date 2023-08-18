2b29a89acdf8456ba579d6446789031c86a0ab7f
Your Name <gilbertozioma0@gmail.com>
2023-08-18 14:32:56 +0100

    bg-dark, back button

--- resources/views/product/update.blade.php
+++ resources/views/product/update.blade.php
@@ -4,7 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="mt-5 mb-3 clearfix">
+                <a href="{{ url('/') }}" class=" btn btn-sm btn-outline-primary float-end"><i class="fa fa-arrow-left"></i> Back</a>
+            </div>
+            <div class="card bg-dark text-light border-secondary">
                 <div class="card-header">{{ __('Update Product') }}</div>
 
                 <div class="card-body">
@@ -14,7 +17,7 @@
 
                         <div class="form-group mb-3">
                             <label for="name">{{ __('Name') }}</label>
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $product->name) }}" required autocomplete="name" autofocus>
+                            <input id="name" type="text" class="form-control bg-dark text-light border-secondary @error('name') is-invalid @enderror" name="name" value="{{ old('name', $product->name) }}" required autocomplete="name" autofocus>
                             @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -24,7 +27,7 @@
 
                         <div class="form-group mb-3">
                             <label for="description">{{ __('Description') }}</label>
-                            <textarea id="description" class="form-control @error('description') is-invalid @enderror" name="description" required>{{ old('description', $product->description) }}</textarea>
+                            <textarea id="description" class="form-control bg-dark text-light border-secondary @error('description') is-invalid @enderror" name="description" required>{{ old('description', $product->description) }}</textarea>
                             @error('description')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -34,7 +37,7 @@
 
                         <div class="form-group mb-3">
                             <label for="price">{{ __('Price') }}</label>
-                            <input id="price" type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price', $product->price) }}" required>
+                            <input id="price" type="number" step="0.01" class="form-control bg-dark text-light border-secondary @error('price') is-invalid @enderror" name="price" value="{{ old('price', $product->price) }}" required>
                             @error('price')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -42,9 +45,9 @@
                             @enderror
                         </div>
 
-                        <div class="form-group mb-3">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Update Product') }}
+                        <div class="form-group mb-3 bg-dark text-light border-secondary">
+                            <button type="submit" class="btn btn-outline-success">
+                                {{ __('Update') }}
                             </button>
                         </div>
                     </form>
