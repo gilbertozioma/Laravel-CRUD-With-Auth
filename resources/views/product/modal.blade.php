95a59ef3e8343fe3363ace93351fad7431d3c11e
Your Name <gilbertozioma0@gmail.com>
2023-08-18 14:30:10 +0100

    btn-outline, product->id

--- resources/views/product/modal.blade.php
+++ resources/views/product/modal.blade.php
@@ -1,5 +1,5 @@
 <!-- Delete Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+<div class="modal fade" id="deleteModal{{ $product->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content bg-dark">
@@ -11,12 +11,12 @@
                 <h5 class="mt-4 text-white">Are you sure you want to delete this Product?</h5>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-sm btn-primary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-sm btn-outline-primary" data-bs-dismiss="modal">Cancel</button>
 
                 <form method="POST" action="{{ url('delete/'.$product->id) }}">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
                 </form>
             </div>
         </div>
