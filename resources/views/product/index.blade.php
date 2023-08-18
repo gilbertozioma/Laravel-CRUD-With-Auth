18e87bd484e7a415e604c79ab070c6a00b86fba6
Your Name <gilbertozioma0@gmail.com>
2023-08-18 14:26:08 +0100

    bg-dark, btn-outline, ID, delete modal

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -12,15 +12,16 @@
                 <div class="alert alert-danger">{{ session('message_error') }}</div>
             @endif
             <h4 class="mb-3">All Products
-                <a class="btn btn-sm btn-success float-end" href="create"><i class="fa fa-plus"></i> Add Product</a>
+                <a class="btn btn-sm btn-outline-success float-end" href="create"><i class="fa fa-plus"></i> Add Product</a>
             </h4>
-            <div class="card">
-                {{-- <div class="card-header">{{ __('All Products') }}</div> --}}
+            <div class="card border border-secondary shadow-sm">
+                <div class="card-header bg-dark text-light">{{ __('All Products') }}</div>
 
-                <div class="card-body">
-                    <table class="table">
+                <div class="card-body bg-dark">
+                    <table class="table table-dark border border-secondary table-striped">
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>Name</th>
                                 <th>Description</th>
                                 <th>Price</th>
@@ -31,13 +32,14 @@
                             
                             @forelse ($products as $product)
                             <tr>
+                                <td>{{ $product->id }}</td>
                                 <td>{{ $product->name }}</td>
                                 <td>{{ $product->description }}</td>
-                                <td>{{ $product->price }}</td>
+                                <td>${{ $product->price }}</td>
                                 <td class="">
-                                    <a href="{{ url('read/'.$product->id) }}" class="btn btn-sm btn-primary text-decoration-none m-1"><i class="fa fa-eye"></i> </a>
-                                    <a href="{{ url('update/'.$product->id.'/edit') }}" class="btn btn-sm btn-success text-decoration-none m-1"><i class="fa fa-pencil"></i> </a>
-                                        <button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                    <a href="{{ url('read/'.$product->id) }}" class="btn btn-sm btn-outline-primary text-decoration-none m-1"><i class="fa fa-eye"></i> </a>
+                                    <a href="{{ url('update/'.$product->id.'/edit') }}" class="btn btn-sm btn-outline-success text-decoration-none m-1"><i class="fa fa-pencil"></i> </a>
+                                        <button class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $product->id }}">
                                             <span><i class="fa-solid fa-trash"></i></span></span>
                                         </button>
                                     </a>
